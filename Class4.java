643bb5781db7ea89d71e6b09ecaeb793e6fbfdd1
Piotr Zdanowski <piotr.zdanowski@outlook.com>
2019-04-18 13:02:48 +0200

    added Class3, Class4 and Class5

---
+++ Class4.java
@@ -0,0 +1,11 @@
+public class Class4{
+	private int x;
+	
+	public Class4(int x){
+		this.x = x;
+	}
+	
+	public int getX(){
+		return x;
+	}
+}
---
+++ Class5.java
@@ -0,0 +1,11 @@
+public class Class5{
+	private int x;
+	
+	public Class5(int x){
+		this.x = x;
+	}
+	
+	public int getX(){
+		return x;
+	}
+}
