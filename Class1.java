200d5866b08aad6172d7e09d434a507e3a6c05fc
Piotr Zdanowski <piotr.zdanowski@outlook.com>
2019-04-18 12:58:52 +0200

    created Main, Class1 and Class2

---
+++ Class1.java
@@ -0,0 +1,11 @@
+public class Class1{
+	private int x;
+	
+	public Class1(int x){
+		this.x = x;
+	}
+	
+	public int getX(){
+		return x;
+	}
+}
---
+++ Class2.java
@@ -0,0 +1,11 @@
+public class Class2{
+	private int y;
+	
+	public Class1(int y){
+		this.y = y;
+	}
+	
+	public int getY(){
+		return y;
+	}
+}
